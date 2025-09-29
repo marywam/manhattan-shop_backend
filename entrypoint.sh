5e32aebe2d140efccaad6e44a5a0c4980f2cbcfd
marywam <maryannewambui0124@gmail.com>
2025-09-29 23:56:50 +0300

    modified entrypoint w2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-
-# Exit on error
 set -e
 
+# Navigate into project directory
+cd shoptech
+
+# Run migrations
 python manage.py migrate --noinput
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Start Gunicorn
 gunicorn shoptech.wsgi:application --bind 0.0.0.0:8000
