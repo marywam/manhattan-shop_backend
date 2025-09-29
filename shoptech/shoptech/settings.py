988e7783435b4a6c4eb70227297cbf791eda7c15
marywam <maryannewambui0124@gmail.com>
2025-09-29 23:16:33 +0300

    added static files

--- shoptech/shoptech/settings.py
+++ shoptech/shoptech/settings.py
@@ -145,8 +145,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
@@ -161,6 +159,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # <-- add this
 
 
 # Mpesa settings
