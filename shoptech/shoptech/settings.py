21320ba58a9f6ef9c0f42719af2457100a3dec43
marywam <maryannewambui0124@gmail.com>
2025-09-29 22:39:10 +0300

    added ssl

--- shoptech/shoptech/settings.py
+++ shoptech/shoptech/settings.py
@@ -105,6 +105,9 @@ DATABASES = {
         "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST", default="127.0.0.1"),
         "PORT": config("DB_PORT", default="5432"),
+        "OPTIONS": {
+            'sslmode': 'require',   
+        },
     }
 }
 
