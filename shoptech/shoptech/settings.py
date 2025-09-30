fafd7f9b79f4c0a3d66d485d770b2eac0fde8f9d
marywam <maryannewambui0124@gmail.com>
2025-10-01 01:24:37 +0300

    improved the settings added SSL

--- shoptech/shoptech/settings.py
+++ shoptech/shoptech/settings.py
@@ -109,9 +109,9 @@ DATABASES = {
         "PASSWORD": config("DB_PASSWORD"),
         "HOST": config("DB_HOST", default="127.0.0.1"),
         "PORT": config("DB_PORT", default="5432"),
-        # "OPTIONS": {
-        #     'sslmode': 'require',   
-        # },
+        "OPTIONS": {
+            'sslmode': 'require',   
+        },
     }
 }
 
